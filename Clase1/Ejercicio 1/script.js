6d2f065d6847a07aee8e2dbda4a3521984914aeb
Mr-Shack <shack687@gmail.com>
2025-03-20 21:19:04 -0600

    Clase 2

--- Clase1/Ejercicio 1/script.js
+++ Clase1/Ejercicio 1/script.js
@@ -1,2 +1,7 @@
 alert("Información de tu mascota");
+let tipoMascota = prompt ("¿Qué tipo de mascota tienes?");
 let nombreMascota = prompt ("Escribe el nombre de tu mascota");
+let edadMascota = prompt ("Escribe la edad de tu mascota");
+let salud = confirm ("¿Tu mascota esta saludable?");
+alert(nombreMascota + " es un " +tipoMascota + " y tiene " + edadMascota + " años");
+console.log("Su mascota es " + salud);
---
+++ Clase1/Ejercicio2/script.js
@@ -0,0 +1,4 @@
+alert ("Responderas a un pequeño cuestionario");
+let nombre = prompt ("¿Cómo te llamas?");
+let edad = prompt ("¿Qué edad tienes?");
+let ocupacion = prompt ("¿Cúal es tu ocupación?");
